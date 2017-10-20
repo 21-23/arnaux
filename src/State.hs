7684c078edb417352e34dbda791dd39668e8d4f1
Alexander Gerasimov <alexanderg@wix.com>
2017-10-20 17:11:57 +0200

    🚧  WIP

--- src/State.hs
+++ src/State.hs
@@ -5,20 +5,35 @@ module State where
 import           Data.Map.Strict    (Map)
 import qualified Data.Map.Strict     as Map
 import           Network.WebSockets (Connection)
+import           Data.UUID          (UUID)
 
 import           Identity           (Identity)
 
-newtype State = State { clients :: Map Identity Connection }
+type ConnectionId = UUID
+
+data ConnectionState
+  = Accepted Connection
+  | CheckedIn Connection Identity
+
+data State = State
+  { connections :: Map ConnectionId ConnectionState
+  , clients :: Map Identity Connection
+  }
 
 empty :: State
 empty = State
-  { clients = Map.empty
+  { connections = Map.empty
+  , clients = Map.empty
   }
 
-addClient :: Connection -> Identity -> State -> State
-addClient connection identity state@(State clients) =
-  state { clients = Map.insert identity connection clients }
-
-removeClient :: Identity -> State -> State
-removeClient identity state@(State clients) =
-  state { clients = Map.delete identity clients }
+addConnection :: ConnectionState -> ConnectionId -> State -> State
+addConnection connectionState connectionId state@(State connections _) =
+  state { connections = Map.insert connectionId connectionState connections }
+--
+-- addClient :: Connection -> Identity -> State -> State
+-- addClient connection identity state@(State clients) =
+--   state { clients = Map.insert identity connection clients }
+--
+-- removeClient :: Identity -> State -> State
+-- removeClient identity state@(State clients) =
+--   state { clients = Map.delete identity clients }
