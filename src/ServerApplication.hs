8d2bb0353bff25558d73218f27721eca802535f8
DaQuirm <leo.daquirm@gmail.com>
2017-11-27 21:43:19 +0100

    📄  Add perfunctory file logging

--- src/ServerApplication.hs
+++ src/ServerApplication.hs
@@ -14,13 +14,14 @@ import           Data.Text                        (pack)
 import qualified Data.Text.IO                     as TextIO
 import qualified Data.UUID.V4                     as UUID
 import qualified Network.WebSockets               as WebSocket
+import           System.Logger                    (Logger, Level(Info, Warn))
 
 
 import           Connection                       (Connection (Connection), ConnectionState (Accepted, CheckedIn))
 import           Effect                           (Effect (Log, Send), handle)
 import           Envelope                         (Envelope (Envelope))
 import           Message                          (IncomingMessage (CheckIn, CheckOut, Message))
-import           Query                            (success, failure)
+import           Query                            (failure, success)
 import           State                            (State)
 import qualified State
 import           StateQuery                       (ServiceError (CheckOutWrongIdentity),
@@ -41,17 +42,17 @@ stateLogic :: StateQuery Action Effect
 stateLogic (Connect connection) = update $> effect
   where
     update = StateT.modify $ State.connect connection
-    effect = Log $ "New connection: " <> pack (show connection)
+    effect = Log Info $ "New connection: " <> pack (show connection)
 
 stateLogic (Incoming connection (Envelope _ (CheckIn identity)) _) = do
   connectionState <- connected connection
   notCheckedInYet connectionState
   identityIsAvailable identity
   StateT.modify $ State.checkIn connection identity
-  success $ Log $ "CheckIn: "
-                  <> pack (show identity)
-                  <> " "
-                  <> pack (show connection)
+  success $ Log Info $ "CheckIn: "
+                    <> pack (show identity)
+                    <> " "
+                    <> pack (show connection)
 
 stateLogic (Incoming connection (Envelope _ (CheckOut identity)) _) = do
   connectionState <- connected connection
@@ -61,10 +62,10 @@ stateLogic (Incoming connection (Envelope _ (CheckOut identity)) _) = do
     then failure $ CheckOutWrongIdentity identity checkedInIdentity
     else do
       StateT.modify $ State.checkOut identity
-      success $ Log $ "CheckOut: "
-                   <> pack (show identity)
-                   <> " "
-                   <> pack (show connection)
+      success $ Log Info $ "CheckOut: "
+                        <> pack (show identity)
+                        <> " "
+                        <> pack (show connection)
 
 stateLogic (Incoming connection (Envelope identity Message) messageString) = do
   connectionState <- connected connection
@@ -75,36 +76,36 @@ stateLogic (Incoming connection (Envelope identity Message) messageString) = do
 stateLogic (Disconnect connection exception) = do
   connectionState <- connected connection
   let effect = case connectionState of
-                Accepted           -> Log $ "Disconnected: "
-                                         <> pack (show connection)
-                                         <> " "
-                                         <> pack (show exception)
+                Accepted           -> Log Info $ "Disconnected: "
+                                              <> pack (show connection)
+                                              <> " "
+                                              <> pack (show exception)
 
-                CheckedIn identity -> Log $ "Checked out & disconnected: "
-                                         <> pack (show identity)
-                                         <> " "
-                                         <> pack (show exception)
+                CheckedIn identity -> Log Info $ "Checked out & disconnected: "
+                                              <> pack (show identity)
+                                              <> " "
+                                              <> pack (show exception)
   success effect
 
-updateState :: MVar State -> Action -> IO ()
-updateState stateVar action = do
+updateState :: Logger -> MVar State -> Action -> IO ()
+updateState logger stateVar action = do
   state <- takeMVar stateVar
   case StateT.runStateT (stateLogic action) state of
     Left err -> do
       putMVar stateVar state
-      print err
+      handle logger $ Log Warn $ pack $ show err
     Right (effect, newState) -> do
       putMVar stateVar newState
-      handle effect
+      handle logger effect
 
-application :: MVar State -> WebSocket.ServerApp
-application stateVar pending = do
+application :: Logger -> MVar State -> WebSocket.ServerApp
+application logger stateVar pending = do
   wsConnection <- WebSocket.acceptRequest pending
   WebSocket.forkPingThread wsConnection 30 --seconds
 
   connectionId <- UUID.nextRandom
   let connection = Connection connectionId wsConnection
-  updateState stateVar (Connect connection)
+  updateState logger stateVar (Connect connection)
 
   catch (forever
     (do
@@ -112,6 +113,6 @@ application stateVar pending = do
       case eitherDecode string :: Either String (Envelope IncomingMessage) of
         Right envelope ->
           let action = Incoming connection envelope string
-           in updateState stateVar action
+           in updateState logger stateVar action
         Left err -> TextIO.putStrLn $ "Message parsing error " <> pack err))
-    (updateState stateVar . Disconnect connection)
+    (updateState logger stateVar . Disconnect connection)
--- src/WebSocketServer.hs
+++ src/WebSocketServer.hs
@@ -3,17 +3,19 @@
 module WebSocketServer (startServer) where
 
 import           Control.Concurrent             (newMVar)
-import qualified Network.WebSockets              as WebSocket
-import           Network.Wai                    (Application, responseLBS)
-import qualified Network.Wai.Handler.Warp        as Warp
-import           Network.Wai.Handler.WebSockets (websocketsOr)
-import           Network.HTTP.Types             (status400)
 import           Data.Monoid                    ((<>))
+import           Network.HTTP.Types             (status400)
+import           Network.Wai                    (Application, responseLBS)
+import qualified Network.Wai.Handler.Warp       as Warp
+import           Network.Wai.Handler.WebSockets (websocketsOr)
+import qualified Network.WebSockets             as WebSocket
+import qualified System.Logger                  as Logger
+import qualified System.Envy                    as Envy
 
+import           ServerApplication              (application)
 import qualified State
 import           ServerApplication              (application)
 import           Config                         (Config(Config))
-import qualified System.Envy                    as Envy
 
 backupApp :: Application
 backupApp _ respond =
@@ -25,8 +27,11 @@ startServer = do
   case envConfig of
     Left err -> putStrLn $ "Config error:" <> err
     Right (Config port _) -> do
+      let infoMessage = "🕊  Listening on port " <> show port
+      putStrLn infoMessage
       stateVar <- newMVar State.empty
-      putStrLn $ "🕊  Listening on port " <> show port
+      logger <- Logger.create $ Logger.Path "logs/arnaux.log"
+      Logger.info logger $ Logger.msg infoMessage
       Warp.run port $ websocketsOr WebSocket.defaultConnectionOptions
                                    (application stateVar)
                                    backupApp
